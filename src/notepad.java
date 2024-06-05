83cb692404ebaf338c996d6216a2e8eb59744db8
Nese <neseilhannn@gmail.com>
2024-06-05 22:30:48 +0300

    arrayList feature added.

--- src/notepad.java
+++ src/notepad.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
 
@@ -31,11 +32,22 @@ public class notepad {
         catch (IOException e){
             e.getStackTrace();
         }
+
+
         try {
             FileReader fileReader = new FileReader("notlar.txt");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String okunanMetin = bufferedReader.readLine();
 
+            // ------ OPSİYONEL --------
+            ArrayList<String> metinListesi = new ArrayList<>(); //saklamak icin bir arrayList olustur
+            while (okunanMetin != null) { //dosyadan okunan verileri boş degilse arrayList'e ekle
+                metinListesi.add(okunanMetin);
+                okunanMetin = bufferedReader.readLine();
+            }
+            for (String satir : metinListesi) {
+                System.out.println(satir);
+            }
             bufferedReader.close();
         }
         catch(IOException e){
